5434110608dc1b6e1d56db52cbbd6702e2e41c65
Dinh Tan Hieu <dinhtanhieu2005@gmail.com>
2026-05-03 12:21:35 +0700

    test: configure jest to run unit tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!react-native|@react-native|expo|@expo|@react-navigation)'
-  ],
+  preset: "jest-expo",
+
+  // Keep coverage collection consistent with package.json
   collectCoverage: true,
-  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    "**/*.{ts,tsx,js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/expo-env.d.ts",
+    "!**/.expo/**",
+    "!**app-examples/**",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text"],
+
+  // Transform ignore: allow some react-native/expo packages to be transformed
+  transformIgnorePatterns: [
+    "node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg))",
+  ],
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  testPathIgnorePatterns: ["/node_modules/", "/android/", "/ios/"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
 };
